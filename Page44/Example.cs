ad89b7ae1467772720de17647a146526f94cb8bd
rodionzam <rodionzam@yandex.ru>
2021-09-12 01:35:00 +0300

    LB The first program

---
+++ Page44/Example.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Это простая программа на C#.
+ * Назовём её Example.cs.
+ */
+
+using System;
+
+class Example
+{
+    //Любая программа на C# начинается с вызова метода Main().
+    static void Main()
+    {
+        Console.WriteLine("Простая программа на C#.");
+    }
+}
