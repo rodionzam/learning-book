33466a15b343f2acaa63aeeb37ceffc1a6abd254
rodionzam <rodionzam@yandex.ru>
2021-09-12 16:38:46 +0300

    LB for cycle and code block

---
+++ Page60/ForDemo.cs
@@ -0,0 +1,15 @@
+﻿//Продемонстрировать применение оператора цикла for.
+
+using System;
+
+class ForDemo
+{
+    static void Main()
+    {
+        int count;
+        for(count = 0; count < 5; count++)
+            Console.WriteLine("Это подсчёт: " + count);
+        
+        Console.WriteLine("Готово!");
+    }
+}
---
+++ Page62/BlockDemo.cs
@@ -0,0 +1,22 @@
+﻿//Продемонстрировать применение кодового блока.
+
+using System;
+
+class BlockDemo
+{
+    static void Main()
+    {
+        int i, d, j;
+
+        i = 5;
+        j = 10;
+        
+        //Адресатом этого оператора if служит кодовый блок.
+        if (i != 0)
+        {
+            Console.WriteLine("i не равно нулю");
+            d = j / i;
+            Console.WriteLine("j / i равно " + d);
+        }
+    }
+}
