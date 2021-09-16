028cbc9e6ba42f5e05c00543a9e9e87482f35941
rodionzam <rodionzam@yandex.ru>
2021-09-16 23:00:20 +0300

    LB sum in code block and type data

---
+++ Page63/ProdSum.cs
@@ -0,0 +1,23 @@
+﻿//Вычислить сумму и произведение чисел от 1 до 10.
+
+using System;
+
+class ProdSum
+{
+    static void Main()
+    {
+        int prod, sum, i;
+
+        sum = 0;
+        prod = 1;
+
+        for (i = 1; i <= 10; i++)
+        {
+            sum += i;
+            prod *= i;
+        }
+
+        Console.WriteLine("Сумма равна " + sum);
+        Console.WriteLine("Произведение равно " + prod);
+    }
+}
---
+++ Page72/Trigonometry.cs
@@ -0,0 +1,19 @@
+﻿//Продемонстрировать применение тригонометрических функций.
+
+using System;
+
+class Trigonometry 
+{
+    static void Main()
+    {
+        Double theta; //угол в радианах
+
+        for (theta = 0.1; theta <= 1.0; theta = theta + 0.1)
+        {
+            Console.WriteLine("Синус угла " + theta + " равен " + Math.Sin(theta));
+            Console.WriteLine("Косинус угла " + theta + " равен " + Math.Cos(theta));
+            Console.WriteLine("Тангенс угла " + theta + " равен " + Math.Tan(theta));
+            Console.WriteLine();
+        }
+    }
+}
